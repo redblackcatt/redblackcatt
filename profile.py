a3f0df36ff12f3ccf1a442ac9a8039d424470289
wilburie <133293718+redblackcatt@users.noreply.github.com>
2023-07-19 12:58:29 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,17 +1,1 @@
-hello = '''
-Hello there, I'm redblackcatt, a C, C++, and Python developer.
-I like to make programs like these:
 
-1. Simple games, like Tic-Tac-Toe or Poker.
-2. Make super cool Homebrew apps for the 3DS.
-3. Take simple program requests!
-
-However, it is important to note I'm not exactly an expert!
-I'm just here for fun! :)
-
-See you!
-
-- redblackcatt
-'''
-
-print(hello)
